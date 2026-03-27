04223cf2d4cc24935635e64a9f1f77e299d668ad
Karan Gattani <karangattani07@gmail.com>
2026-03-27 17:08:02 +0530

    Add main.jsx to render LocalFlash component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LocalFlash from "./LocalFlash.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LocalFlash />
+  </React.StrictMode>
+);
