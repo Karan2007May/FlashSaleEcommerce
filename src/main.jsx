81bd197b47675a90cd670d07990fd7645a61c58d
Karan Gattani <karangattani07@gmail.com>
2026-03-27 17:44:02 +0530

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LocalFlash from "./LocalFlash.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LocalFlash />
-  </React.StrictMode>
-);
